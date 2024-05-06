0f62be61462206fe7f8c3b4ee0062edc2bc81d37
Hélio Dantas <helio_dantas@outlook.com>
2024-05-06 10:53:16 -0300

    feat: iniciando api gateway

--- src/lambda_login.tf
+++ src/lambda_login.tf
@@ -66,12 +66,15 @@ resource "aws_lambda_function" "login" {
   function_name    = "${data.terraform_remote_state.infra.outputs.resource_prefix}-lambda-login"
   filename         = "files/login_lambda_function_payload.zip"
   role             = aws_iam_role.lambda.arn
+  # role             = "arn:aws:iam::364764462991:role/LabRole"
   handler          = "index.handler"
   timeout          = 5
   memory_size      = 128
   source_code_hash = data.archive_file.lambda_login_artefact.output_base64sha256
   depends_on = [
     aws_cloudwatch_log_group.lambda_log_group,
+    # aws_cognito_user_pool.default,
+    # aws_cognito_user_pool_client.default
   ]
 
   runtime = var.lambda_login_runtime
@@ -91,10 +94,82 @@ resource "aws_lambda_function" "login" {
       DB_DATABASE = "${data.terraform_remote_state.database.outputs.db_name}",
       DB_USERNAME = "${var.db_user}",
       DB_PASSWORD = "${var.db_password}",
+      # USER_POOL_ID = "${aws_cognito_user_pool.default.id}",
+      # CLIENT_ID = "${aws_cognito_user_pool_client.id}",
     }
   }
 
   tags = {
     Name = "${data.terraform_remote_state.infra.outputs.resource_prefix}-lambda"
   }
+}
+
+
+# Integre a função Lambda ao método da API Gateway
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id             = data.terraform_remote_state.infra.outputs.gateway_id
+  resource_id             = data.terraform_remote_state.infra.outputs.gateway_resource_id
+  http_method             = data.terraform_remote_state.infra.outputs.gateway_http_method
+  
+  integration_http_method = "GET"
+  type                    = "AWS"
+  uri                     = "${aws_lambda_function.login.invoke_arn}"
+}
+
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id             = data.terraform_remote_state.infra.outputs.gateway_id
+  resource_id             = data.terraform_remote_state.infra.outputs.gateway_resource_id
+  http_method             = data.terraform_remote_state.infra.outputs.gateway_http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id             = data.terraform_remote_state.infra.outputs.gateway_id
+  resource_id             = data.terraform_remote_state.infra.outputs.gateway_resource_id
+  http_method             = data.terraform_remote_state.infra.outputs.gateway_http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.lambda
+  ]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+  ]
+
+  rest_api_id = data.terraform_remote_state.infra.outputs.gateway_id
+  stage_name = "dev"
+}
+
+
+# resource "aws_iam_role_policy_attachment" "lambda_basic" {
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# //role = aws_iam_role.lambda_role.name
+#   role = "arn:aws:iam::364764462991:role/LabRole"
+# }
+
+# Conecte o método à função Lambda
+resource "aws_lambda_permission" "fiap-gateway_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.login.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # Substitua "fiap_gateway_id" pelo ID da sua API Gateway
+  source_arn = "${data.terraform_remote_state.infra.outputs.gateway_execution_arn}/*/*/login"
 }
